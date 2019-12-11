d110b8d7823a32f1abee7f824baf47dfc8b76f73
Vasyl <vasyl.boychuk@hetzner.co.za>
2019-12-11 21:51:42 +0200

    minor

--- README.md
+++ README.md
@@ -4,7 +4,11 @@ gem install calculator_gem
 
 ## Usage example
 ```
+irb
+
 require 'calculator_gem'
+
+# print net_amount, commission_amount
 pp CalculatorGem::Calculator.call(
   amount: 200,
   commission_amount: 10, commission_percent: 20
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -1,25 +1,34 @@
 describe CalculatorGem::Calculator do
   context 'when commission is round number' do
-    it 'calculates netto_amount and commission' do
-      netto_amount, commission = described_class.call(
+    it 'calculates net_amount and commission' do
+      net_amount, commission = described_class.call(
         amount: 100,
         commission_amount: 1, commission_percent: 20
       )
 
-      expect(netto_amount).to eq(79)
+      expect(net_amount).to eq(79)
       expect(commission).to eq(21)
     end
   end
 
   context 'when commission is not round number' do
-    it 'calculates netto_amount and commission' do
-      netto_amount, commission = described_class.call(
+    it 'calculates net_amount and commission' do
+      net_amount, commission = described_class.call(
         amount: 10,
         commission_amount: 1.25, commission_percent: 28
       )
 
-      expect(netto_amount).to eq(5.95)
+      expect(net_amount).to eq(5.95)
       expect(commission).to eq(4.05)
+    end
+  end
+
+  context 'when only amount provided' do
+    it 'calculates net_amount and commission' do
+      net_amount, commission = described_class.call(12.34)
+
+      expect(net_amount).to eq(12.34)
+      expect(commission).to eq(0)
     end
   end
 end
