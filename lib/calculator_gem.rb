76d3983f426712738c4ca13cda8f0c17c6b07794
Vasyl <vasyl.boychuk@hetzner.co.za>
2019-12-12 14:49:27 +0200

    fixing default amount and entitiy

--- lib/calculator_gem.rb
+++ lib/calculator_gem.rb
@@ -3,25 +3,31 @@ require_relative 'entities/user'
 
 module CalculatorGem
   class Calculator
+    DEFAULT_COMMISSION_AMOUNT = 15
+    DEFAULT_COMMISSION_PERCENT = 35
+
     def self.call(amount:,
-                  commission_amount: 0, commission_percent: 0,
+                  commission_amount: nil, commission_percent: nil,
                   commission_entity: nil
       )
 
       new(amount: amount,
         commission_amount: commission_amount, commission_percent: commission_percent,
-        commission_entity: commission_entity).calculate
+        commission_entity: commission_entity
+      ).calculate
     end
 
     def initialize(amount:,
-                   commission_amount: 0, commission_percent: 0,
+                   commission_amount: nil, commission_percent: nil,
                    commission_entity: nil
       )
 
       @amount = amount
+
       @commission_amount = commission_amount
       @commission_percent = commission_percent
       apply_commission_entity(commission_entity)
+      apply_default_commission
     end
 
     def calculate
@@ -37,8 +43,13 @@ module CalculatorGem
     def apply_commission_entity(commission_entity)
       return unless commission_entity
 
-      @commission_amount += commission_entity.commission_amount
-      @commission_percent += commission_entity.commission_percent
+      @commission_amount = commission_entity.commission_amount
+      @commission_percent = commission_entity.commission_percent
+    end
+
+    def apply_default_commission
+      @commission_amount ||= DEFAULT_COMMISSION_AMOUNT
+      @commission_percent ||= DEFAULT_COMMISSION_PERCENT
     end
   end
 end
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -24,32 +24,41 @@ describe CalculatorGem::Calculator do
   end
 
   context 'when only amount provided' do
-    it 'calculates net_amount and commission' do
-      net_amount, commission = described_class.call(amount: 12.34)
+    it 'uses default commission' do
+      net_amount, commission = described_class.call(amount: 200)
 
-      expect(net_amount).to eq(12.34)
-      expect(commission).to eq(0)
+      expect(net_amount).to eq(115.00)
+      expect(commission).to eq(85.00)
     end
   end
 
   context 'when product entity provided' do
     let(:product) { CalculatorGem::Product.new(commission_amount: 5, commission_percent: 10) }
-    context 'when only amount provided' do
-      it 'calculates net_amount and commission' do
+    context 'when no commission arguments provided' do
+      it 'calculates values' do
+        net_amount, commission = described_class.call(amount: 100, commission_entity: product)
+
+        expect(net_amount).to eq(85)
+        expect(commission).to eq(15)
+      end
+    end
+
+    context 'when commission arguments provided' do
+      it 'does not pay attention on commission arguments' do
         net_amount, commission = described_class.call(amount: 100,
           commission_amount: 10, commission_percent: 20,
           commission_entity: product
         )
 
-        expect(net_amount).to eq(55)
-        expect(commission).to eq(45)
+        expect(net_amount).to eq(85)
+        expect(commission).to eq(15)
       end
     end
   end
 
   context 'when user entity provided' do
     let(:user) { CalculatorGem::User.new(commission_amount: 5, commission_percent: 10) }
-    context 'when only amount provided' do
+    context 'when only commission entity provided' do
       it 'calculates net_amount and commission' do
         net_amount, commission = described_class.call(amount: 100,
           commission_entity: user
